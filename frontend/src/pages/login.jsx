88b7895a6777661ee6c2a0a0cf1f0cc29fc8b6fc
josephErian <gozefatef1999@gmail.com>
2023-11-30 18:45:18 +0200

    react hooks login and register

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
-const Login = () => {
+const Login = () => { 
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword,setLoginPassword] = useState('');
+
+    const onEmailChange = (e) =>{
+        setLoginEmail(e.target.value);
+    }
+
+    const onPasswordChange = (e) =>{
+        setLoginPassword(e.target.value);
+    }
+
+    const onSubmitClick = () =>(
+        alert(`logging in ${loginEmail} with password ${loginPassword}`)
+    )
   return (
     <div className="flex justify-center items-center h-screen">
     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -12,8 +27,10 @@ const Login = () => {
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="email"
-          type="text"
+          type="email"
+          name="email"
           placeholder="Username or Email Address"
+          onChange={onEmailChange}
         />
       </div>
       <div className="mb-4">
@@ -25,12 +42,16 @@ const Login = () => {
           id="password"
           type="password"
           placeholder="Password"
+          name="password"
+          onChange={onPasswordChange}
         />
       </div>
       <div className="mb-6 flex flex-row items-center space-between">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-3 rounded focus:outline-none focus:shadow-outline"
           type="button" 
+          name="submit"
+          onClick={onSubmitClick}
         >
           Login
         </button><h5 className="text-12">Create New One <Link to="/register" className="underline font-bold">Register</Link></h5>
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 const Register = () => {
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword,setLoginPassword] = useState('');
+
+    const onEmailChange = (e) =>{
+        setLoginEmail(e.target.value);
+    }
+
+    const onPasswordChange = (e) =>{
+        setLoginPassword(e.target.value);
+    }
+
+    const onSubmitClick = () =>(
+        alert(`logging in ${loginEmail} with password ${loginPassword}`)
+    )
   return (
   <div className="flex justify-center items-center h-screen">
     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -12,8 +27,10 @@ const Register = () => {
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="email"
-          type="text"
+          type="email"
+          name="email"
           placeholder="Username or Email Address"
+          onChange={onEmailChange}
         />
       </div>
       <div className="mb-4">
@@ -24,13 +41,16 @@ const Register = () => {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
           type="password"
+          name="password"
           placeholder="Password"
+          onChange={onPasswordChange}
         />
       </div>
       <div className="mb-6 flex flex-row items-center space-between">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-3 rounded focus:outline-none focus:shadow-outline"
           type="button" 
+          onClick={onSubmitClick}
         >
           Register
         </button><h5 className="text-12">have an account <Link to="/login" className="underline font-bold">Login</Link></h5>
