f2cb02a87d66dff3b0b8c85a2254397bc180f0de
josephErian <gozefatef1999@gmail.com>
2023-12-09 19:44:00 +0200

    finsih loginRoute

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const app = express();
 const { handleSignin } = require('./handleLogin');
 const { handleRegister } = require('./handleRegister');
 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(cors({
+    origin:'http://localhost:3000',
+}))
+
+app.use(express.json())
 
 const db = knex({
     client: 'pg',
@@ -21,27 +28,22 @@ const db = knex({
     }
 });
 
-app.use(cors({
-    origin:'http://localhost:3000',
-}))
 
-app.use(express.static(path.join(__dirname,'public')));
-const PORT = process.env.PORT || 8000
 
-app.use(express.json())
 
+app.post('/Register',(req,res)=> handleRegister(req,res,db,bcrypt));
+app.post('/login', (req,res)=> handleSignin(req,res,db,bcrypt));
 
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'))
     })
-    
-app.post('/Register',(req,res)=> handleRegister(req,res,db,bcrypt));
-app.post('/login', (req,res)=> handleSignin(req,res,db,bcrypt));
-    
+
 const server = require('https').createServer({
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 },app);
+
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, ()=>{
     console.log(`server is listening on port : ${PORT}`)
